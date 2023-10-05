4e91de1b6effdd5f149b4d64fc59e7e1ef7456e1
Kristina Safonova <kristinekleine@inbox.lv>
2023-10-05 21:00:16 +0300

    adding registration form + bootstrap

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,11 +8,13 @@ use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public const ROLE_USER = 'ROLE_USER';
@@ -70,6 +72,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\ManyToMany(targetEntity: Shop::class, inversedBy: 'user')]
     #[ORM\JoinTable(name: "user_to_shop")]
     private Collection $shops;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -291,6 +296,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         if ($this->shops->removeElement($shop)) {
             $shop->removeUser($this);
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
